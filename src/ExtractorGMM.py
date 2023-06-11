5c9afc02885f37c5f0d72c4d5683562036952233
WillianaLeite <willianaluziasousaleite@gmail.com>
2023-06-11 17:56:46 -0300

    Add all docstrings

--- src/ExtractorGMM.py
+++ src/ExtractorGMM.py
@@ -1,74 +1,120 @@
 import numpy as np
 from numpy import ndarray
 import pandas as pd
+import yaml
 from src.GaussianMixtureModel import GaussianMixtureModel
+from logging import Logger
+import os
 
-N_COMPONENTS_PER_CLASS = {0:2, 1:2, 2:3, 3:2, 4:2, 5:3, 6:4}
-BACKGROUND = list(range(0, 2))
-CEREBROSPINAL_FLUID = list(range(0, 6))
-STROKE_ISCHEMIC = list(range(6, 22))
-WHITE_MATTER = list(range(23, 35))
-GRAY_MATTER = list(range(35, 41))
-STROKE_HEMORRHAGIC = list(range(50, 81))
-CALCIFICATION = list(range(130, 250)) 
-
-def _get_region_number(hu_value):
-    if hu_value in BACKGROUND: 
-        return 0
-    elif hu_value in CEREBROSPINAL_FLUID: 
-        return 1
-    elif hu_value in STROKE_ISCHEMIC: 
-        return 2
-    elif hu_value in WHITE_MATTER: 
-        return 3
-    elif hu_value in GRAY_MATTER: 
-        return 4
-    elif hu_value in STROKE_HEMORRHAGIC: 
-        return 5
-    elif hu_value in CALCIFICATION: 
-        return 6
 
 class ExtractorGMM:
-    
-    def __init__(self, image: ndarray, pixel_level_feature: bool):
 
-        '''
-        Constructor that receives the image and chooses the output format of the features
-        
-        Parameters:
-            image: The input image
-            pixel_level_feature: When true, the features will be at the pixel level and when false, they will be percentage.
-        '''
+    """
+    The instance extracts the features related to the brain regions of a hemisphere from the tomography.
+    The features represent the segmentation of the brain in the regions defined in the configuration file.
+
+    :param image: The input image.
+    :type image: ndarray
+
+    :param pixel_level_feature: If true, the segmentation returns the brain region of each pixel (MGABTD-pixel extractor).
+                                If false returns the percentage of pixels in each brain region (MGABTD-percent).
+    :type pixel_level_feature: bool
+
+    :param logger: The main logging.
+    :type logger: Logger
+    """
+    
+    def __init__(self, image: ndarray, pixel_level_feature: bool, logger: Logger):
 
         self.image = image
         self.pixel_level_feature = pixel_level_feature
+        self.logger = logger
+        self.read_config()
         self.fit()
-    
+
+    def read_config(self):
+
+        """ This function determines the value of the constants used to determine brain regions and to train the GMM. """
+
+        try:
+            with open(os.getcwd() + '/src/config.yml', 'r') as file:
+                config = yaml.safe_load(file)
+
+            self.N_ITER = int(config.get('N_ITER'))
+
+            self.N_COMPONENTS_PER_CLASS = {}
+            for c in config.get('N_COMPONENTS_PER_CLASS').split(', '):
+                class_ = int(c.split('-')[0])
+                n_componets = int(c.split('-')[1])
+                self.N_COMPONENTS_PER_CLASS[class_] = n_componets
+
+            self.NUMBER_REGIONS = len(self.N_COMPONENTS_PER_CLASS.keys())
+
+            background_threshold = config.get('BACKGROUND').split('-')
+            self.BACKGROUND = list(range(int(background_threshold[0]), int(background_threshold[1]) + 1))
+
+            cerebrospinal_fluid_threshold = config.get('CEREBROSPINAL_FLUID').split('-')
+            self.CEREBROSPINAL_FLUID = list(range(int(cerebrospinal_fluid_threshold[0]), int(cerebrospinal_fluid_threshold[1]) + 1))
+
+            stroke_ischemic_threshold = config.get('STROKE_ISCHEMIC').split('-')
+            self.STROKE_ISCHEMIC = list(range(int(stroke_ischemic_threshold[0]), int(stroke_ischemic_threshold[1]) + 1))
+
+            white_matter_threshold = config.get('WHITE_MATTER').split('-')
+            self.WHITE_MATTER = list(range(int(white_matter_threshold[0]), int(white_matter_threshold[1]) + 1))
+
+            gray_matter_threshold = config.get('GRAY_MATTER').split('-')
+            self.GRAY_MATTER = list(range(int(gray_matter_threshold[0]), int(gray_matter_threshold[1]) + 1))
+
+            stroke_hemorrhagic_threshold = config.get('STROKE_HEMORRHAGIC').split('-')
+            self.STROKE_HEMORRHAGIC = list(range(int(stroke_hemorrhagic_threshold[0]), int(stroke_hemorrhagic_threshold[1]) + 1))
+            
+            calcification_threshold = config.get('CALCIFICATION').split('-')
+            self.CALCIFICATION = list(range(int(calcification_threshold[0]), int(calcification_threshold[1]) + 1))
+            self.logger.info('Configuration variables read from configuration file')
+
+        except:
+            
+            self.logger.warning('There is something wrong with the configuration file, read default values')
+            self.N_COMPONENTS_PER_CLASS = {0:2, 1:2, 2:3, 3:2, 4:2, 5:3, 6:4}
+            self.N_ITER = 5
+            self.NUMBER_REGIONS = 7
+            self.BACKGROUND = list(range(0, 2))
+            self.CEREBROSPINAL_FLUID = list(range(0, 6))
+            self.STROKE_ISCHEMIC = list(range(6, 22))
+            self.WHITE_MATTER = list(range(23, 35))
+            self.GRAY_MATTER = list(range(35, 41))
+            self.STROKE_HEMORRHAGIC = list(range(50, 81))
+            self.CALCIFICATION = list(range(130, 250))
+
+
     def fit(self):
 
-        ''' Training a GMM for classification using brain radiodensity regions as class '''
+        """ Training a GMM for classification using brain radiodensity regions as class. """
 
-        all_regions = BACKGROUND + CEREBROSPINAL_FLUID + STROKE_ISCHEMIC + WHITE_MATTER + GRAY_MATTER + STROKE_HEMORRHAGIC + CALCIFICATION
-
-        hu_values = list(set(all_regions))
-        df_train = pd.DataFrame(hu_values, columns=['hu_values'])
-        df_train['brain_region'] = df_train['hu_values'].apply(_get_region_number)
+        list_rows = (
+            list(zip([0] * len(self.BACKGROUND), self.BACKGROUND)) + 
+            list(zip([1] * len(self.CEREBROSPINAL_FLUID), self.CEREBROSPINAL_FLUID)) +
+            list(zip([2] * len(self.STROKE_ISCHEMIC), self.STROKE_ISCHEMIC)) +
+            list(zip([3] * len(self.WHITE_MATTER), self.WHITE_MATTER)) +
+            list(zip([4] * len(self.GRAY_MATTER), self.GRAY_MATTER)) +
+            list(zip([5] * len(self.STROKE_HEMORRHAGIC), self.STROKE_HEMORRHAGIC)) +
+            list(zip([6] * len(self.CALCIFICATION), self.CALCIFICATION))
+        )
+        df_train = pd.DataFrame(list_rows, columns=['brain_region', 'hu_values'])
+        df_train.drop_duplicates(inplace=True)
     
-        self.gmm = GaussianMixtureModel(N_COMPONENTS_PER_CLASS, 5)
+        self.gmm = GaussianMixtureModel(self.N_COMPONENTS_PER_CLASS, self.N_ITER)
         self.gmm.fit(df_train.drop(['brain_region'], axis=1), df_train['brain_region'])
         
     def segmentation(self):
 
-        '''
-        This function computes the features from a image
-        
-        Returns:
-            Features from a input image
-        '''
+        """ This function segments a brain tomography image into previously defined brain regions. """
         
         all_pixels = self.image.ravel()
         df = pd.DataFrame(all_pixels, columns=['hu_values'])
-        df_predict = self.gmm.predict(df) # Cada linha é um pixel, e as colunas são as classes
+
+        # Each row is a pixel, and the columns are the classes.
+        df_predict = self.gmm.predict(df)
         
         df_probability = df_predict.T
         segment = df_probability.idxmax()
@@ -77,7 +123,7 @@ class ExtractorGMM:
             return segment
         else:
             classes = []
-            for i in range(1, 8): # São 7 classes
+            for i in range(1, self.NUMBER_REGIONS + 1):
                 classes.append(np.count_nonzero(segment == i))
             total = np.count_nonzero(segment != 0)
             probability = [count_classes/total for count_classes in classes]
--- src/GaussianMixtureModel.py
+++ src/GaussianMixtureModel.py
@@ -5,11 +5,24 @@ from sklearn.mixture import GaussianMixture
 
 class GaussianMixtureModel:
 
-    def __init__(self, dict_k, iter=2):
-        self.dict_k = dict_k 
-        self.iter = iter
+    """
+    Pattern classifier that uses the Bayes theorem using the Mixture of Gaussians as a probability estimator.
+    Each class has a Mixture of Gaussians and from the responsibility it is defined to which class the sample belongs.
 
-    def fit(self, X_train, y_train, verbose=False):
+    :param n_components_per_class: Number of Gaussian components each mixture of Gaussians will have in each class.
+    :type n_components_per_class: dict
+
+    :param n_iter: Maximum number of iterations that the Expectation Maximization algorithm will take to position the Gaussian components correctly.
+    :type n_iter: int
+    """
+
+    def __init__(self, n_components_per_class: dict, n_iter: int = 2):
+        self.n_components_per_class = n_components_per_class 
+        self.n_iter = n_iter
+
+    def fit(self, X_train: pd.DataFrame, y_train: pd.Series, verbose: bool = False):
+
+        """ Training the classifier from a set of samples with their respective classes. """
 
         self.df_train = X_train.copy()
         self.df_train['target'] = y_train
@@ -18,7 +31,7 @@ class GaussianMixtureModel:
         self.dict_priori = {}
         self.dict_gmm = {}
         self.dict_coef_mixture = {}
-        for class_, k in self.dict_k.items():
+        for class_, k in self.n_components_per_class.items():
 
             df_class = self.df_train[self.df_train['target'] == class_]
             coef_mixture = 1/k
@@ -27,7 +40,7 @@ class GaussianMixtureModel:
                 n_components = k, 
                 init_params = 'kmeans', 
                 covariance_type = 'full', 
-                n_init = self.iter, 
+                n_init = self.n_iter, 
                 random_state = 42
             ) 
 
@@ -41,19 +54,21 @@ class GaussianMixtureModel:
             self.dict_priori[class_] = len(df_class) / len(self.df_train)
             self.dict_coef_mixture[class_] = coef_mixture
 
-    def predict(self, df):
+    def predict(self, df: pd.DataFrame):
+        
+        """ Given a set of input samples returns the class of each sample. """
         
         df_metrics = pd.DataFrame(df.reset_index(drop=True).index, columns=['index'])
         df_metrics.set_index('index')
         for class_ in sorted(self.list_class):
-            for component in range(self.dict_k[class_]):
+            for component in range(self.n_components_per_class[class_]):
                 df_metrics[f'class_{class_}_component_{component}'] = (
                     self.dict_gmm[class_][component].pdf(df) * self.dict_coef_mixture[class_]
                 )
 
         for class_ in sorted(self.list_class):
             df_metrics[f'prob_gmm_class_{class_}'] = df_metrics[[f'class_{class_}_component_{component}' 
-                                                                 for component in range(self.dict_k[class_])]].sum(axis=1)
+                                                                 for component in range(self.n_components_per_class[class_])]].sum(axis=1)
 
             df_metrics[f'posteriori_{class_}'] = (
                 df_metrics[f'prob_gmm_class_{class_}'] * 
